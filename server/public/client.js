5ab83796373a70732728d3e03aa4c0c61f1d6898
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-08 17:25:05 -0400

    got server to finally bring me back a number

--- server/public/client.js
+++ server/public/client.js
@@ -2,4 +2,98 @@ $(document).ready(onReady)
 
 function onReady(){
     console.log('hello from jq!');
+    $('#plus').on('click', add);
+    $('#minus').on('click', subtract);
+    $('#times').on('click', multiply);
+    $('#divide').on('click', divide);
+    $('#equals').on('click', sendToServer);
+}
+
+let equation;
+
+function sendToServer(event){
+    event.preventDefault();
+
+    $.ajax({
+        method: 'POST',
+        url: '/formulate',
+        data: equation
+    }).then(function(response){
+        console.log(response);
+
+         $.ajax({
+        method: 'GET',
+        url: '/formulate'
+    }).then(function(response){
+        console.log(response);
+        $('#result').text(response.result);
+    })
+
+    })
+
+   
+}
+
+function history(){
+    
+}
+
+function add(event){
+    event.preventDefault();
+
+    const num1 = $('#num1').val();
+    const num2 = $('#num2').val();
+    const plus = $('#plus').text();
+
+    equation = {
+        num1: num1,
+        num2: num2,
+        operator: plus
+    };
+
+}
+
+function subtract(event){
+    event.preventDefault();
+
+    const num1 = $('#num1').val();
+    const num2 = $('#num2').val();
+    const minus = $('#minus').text();
+
+    equation = {
+        num1: num1,
+        num2: num2,
+        operator: minus
+    };
+
+}
+
+function multiply(event){
+    event.preventDefault();
+
+    const num1 = $('#num1').val();
+    const num2 = $('#num2').val();
+    const times = $('#times').text();
+
+    equation = {
+        num1: num1,
+        num2: num2,
+        operator: times
+    };
+
+}
+
+function divide(event){
+    event.preventDefault();
+
+    const num1 = $('#num1').val();
+    const num2 = $('#num2').val();
+    const divide = $('#divide').text();
+
+    equation = {
+        num1: num1,
+        num2: num2,
+        operator: divide
+    };
+
 }
--- server/server.js
+++ server/server.js
@@ -7,11 +7,35 @@ const app = express();
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let history = [];
+// let lastOfHistory = history[history.length - 1];
+
+function calculate(){
+    let lastOfHistory = history[history.length - 1];
+    if(lastOfHistory.operator === '+'){
+         return Number(lastOfHistory.num1) + Number(lastOfHistory.num2);
+    }else if(lastOfHistory.operator === '-'){
+        return Number(lastOfHistory.num1) - Number(lastOfHistory.num2);
+    }else if(lastOfHistory.operator === '*'){
+        return Number(lastOfHistory.num1) * Number(lastOfHistory.num2);
+    }else if(lastOfHistory.operator === '/'){
+        return Number(lastOfHistory.num1) / Number(lastOfHistory.num2);
+    }
+}
+
+app.get('/formulate', (req, res) => {
+  let result = calculate();
+    res.send({result});
 
 
+})
 
+app.post('/formulate', (req, res) => {
+    let newItem = req.body;
+    history.push(newItem)
+    console.log(history);
 
-
-
+    res.sendStatus(201);
+})
 
 app.listen(5000, () => {console.log('Server Running on port 5000')});
